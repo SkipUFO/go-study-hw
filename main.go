f82570c339478b1aaed13edeac496e276923d41c
SkipUFO <igor.m.sinitsin@gmail.com>
2019-09-14 08:36:06 +0500

    * Что сделано  - Добавлен вывод ошибок в stderr  - Добавлены exit статусы

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/beevik/ntp"
 )
@@ -9,8 +10,10 @@ import (
 func main() {
 	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
 
 	fmt.Println(currentTime)
+	os.Exit(0)
 }
