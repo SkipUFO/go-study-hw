369215f6958cbfc2d98ae11b32affc991dfc12d9
SkipUFO <igor.m.sinitsin@gmail.com>
2019-09-17 10:47:50 +0500

    * HW2 * Что сделано  - убран лишний код из main.go

--- hw2/main.go
+++ hw2/main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/beevik/ntp"
-)
+import "os"
 
 func main() {
-	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(currentTime)
 	os.Exit(0)
 }
